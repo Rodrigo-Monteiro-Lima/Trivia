a90c0bbba4fc19bc8fb19244516653c8bcb3fc69
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-11-12 18:20:37 -0300

    req 8 e 9

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/Game" component={ Game } />
-        <Route path="/settings" component={ Settings } />
-        <Route path="/Feedback" component={ Feedback } />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/Game" component={ Game } />
+      <Route path="/settings" component={ Settings } />
+      <Route path="/Feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
+    </Switch>
   );
 }
 
