b1ea4bbe55b978023cd74ddeb643c2e16a802eb3
jaidernunes <jaidernf@gmail.com>
2022-11-13 16:25:32 -0300

    Req 19: Done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  // state = {
+  //   newRankObj: {},
+  //   prevRank: [],
+  //   newRank: [],
+  // };
+
+  componentDidMount() {
+    const { name, gravatarEmail, score } = this.props;
+
+    const userEmail = gravatarEmail;
+    const emailHash = md5(userEmail).toString();
+    const gravatarImg = `https://www.gravatar.com/avatar/${emailHash}`;
+
+    const addPlayer = {
+      name,
+      score,
+      gravatarImg,
+    };
+    console.log(addPlayer);
+
+    if (localStorage.getItem('ranking') === null) {
+      localStorage.setItem('ranking', JSON.stringify([]));
+      // localStorage.setItem('ranking', [addPlayer]);
+    }
+    console.log(JSON.parse(localStorage.getItem('ranking')));
+    console.log(localStorage.getItem('ranking'));
+
+    const prevRanking = JSON.parse(localStorage.getItem('ranking'));
+    // const prevRanking = localStorage.getItem('ranking');
+    console.log(JSON.parse(localStorage.getItem('ranking')));
+    // console.log(localStorage.getItem('ranking'));
+
+    localStorage.setItem('ranking', JSON.stringify([...prevRanking, addPlayer]));
+    // localStorage.setItem('ranking', [...prevRanking, addPlayer]);
+    console.log(JSON.parse(localStorage.getItem('ranking')));
+  }
+
   render() {
     const { assertions, score } = this.props;
 
@@ -30,6 +68,8 @@ class Feedback extends Component {
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
   score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
 // history: PropTypes.shape({
 //   push: PropTypes.func,
 // }).isRequired,
@@ -39,6 +79,8 @@ Feedback.propTypes = {
 const mapStateToProps = ({ player }) => ({
   assertions: player.assertions,
   score: player.score,
+  name: player.name,
+  gravatarEmail: player.gravatarEmail,
   // questions: player.questions,
   // selectedAnswer: player.selectedAnswer,
   // error: player.error,
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+// import { connect } from 'react-redux';
 
-export default class Ranking extends React.Component {
+class Ranking extends React.Component {
+  state = { ranking: [] };
+
+  componentDidMount() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const orderedRanking = ranking.sort((a, b) => b.score - a.score);
+    console.log(ranking);
+    console.log(orderedRanking);
+
+    this.setState({
+      ranking: orderedRanking,
+    });
+  }
+
   onClick = () => {
     const { history } = this.props;
     history.push('/');
   };
 
   render() {
+    const { ranking } = this.state;
+
     return (
       <>
         <Header />
@@ -20,6 +36,19 @@ export default class Ranking extends React.Component {
           Home
         </button>
         <h1>Ranking</h1>
+        <div>
+          {ranking.map((player, i) => (
+            <div key={ i }>
+              <img src={ player.gravatarImg } alt={ player.name } />
+              <div data-testid={ `player-name-${i}` }>
+                {player.name}
+              </div>
+              <div data-testid={ `player-score-${i}` }>
+                {player.score}
+              </div>
+            </div>
+          ))}
+        </div>
       </>
     );
   }
@@ -29,4 +58,16 @@ Ranking.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  // name: PropTypes.string.isRequired,
+  // gravatarEmail: PropTypes.string.isRequired,
+  // score: PropTypes.number.isRequired,
 };
+
+// const mapStateToProps = ({ player }) => ({
+//   name: player.name,
+//   gravatarEmail: player.gravatarEmail,
+//   score: player.score,
+// });
+
+// export default connect(mapStateToProps)(Ranking);
+export default Ranking;
